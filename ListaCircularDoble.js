8e7c5afba270a3cf1117125eaa0f1eb34c86e4e2
mario ruano <marioruano25@gmail.com>
2021-12-08 21:36:12 -0600

    add: metodo push y enlace del ultimo con el primero con datos de prueba

--- ListaCircularDoble.js
+++ ListaCircularDoble.js
@@ -1,12 +1,34 @@
 console.log("asdad");
 
 class ListaCircular{
-    Inicio = null
-    Ultimo = null
-    constructor(){
-        
+    inicio = null
+    ultimo = null
+    constructor(){}
+
+    push(dato){
+        if (this.vacio()) {
+            this.inicio = this.ultimo = new NodoDoble(null, dato, null);
+            this.ultimo.anterior = this.inicio;
+            this.ultimo.siguiente = this.inicio;
+        }else{
+            let temp = new NodoDoble(this.ultimo.anterior, dato, this.ultimo.siguiente);
+            this.ultimo.anterior.siguiente = temp
+            this.ultimo.siguiente.anterior = temp
+            this.ultimo = temp
+        }
     }
 
+    imprimir(){
+        let temp = this.inicio;
+        while (temp!= this.ultimo) {
+            console.log(temp.dato);
+            temp = temp.siguiente
+        }
+    }
+
+    vacio(){
+        return this.inicio == null
+    }
 }
 
 class NodoDoble
@@ -14,9 +36,19 @@ class NodoDoble
     anterior = null
     siguiente = null
     dato = null
-    constructor(dato, siguiente){
+
+    constructor(anterior,dato, siguiente){
         this.dato = dato
         this.siguiente = siguiente
+        this.anterior = anterior
     }
 }
+
+const lista = new ListaCircular()
+lista.push(3)
+lista.push(4)
+lista.push(5)
+lista.push(6)
+lista.push(7)
+lista.imprimir()
 
